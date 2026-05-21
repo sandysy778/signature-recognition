7a86eda1a7072f6f735509c96633e92c9e13725f
Sandhya Kumari <sandysy778@gmail.com>
2026-05-20 20:51:56 -0700

    notebook-done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author = "sandhya kumari",
+    author_email = "sandysy778@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
+
+
